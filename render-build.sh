c65673e40300ef3631ac4cc398155d04bcd515b4
antonio59bot <boue.antoine@live.fr>
2025-07-18 23:22:56 +0200

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "===== INSTALLING DEPENDENCIES ====="
 pip uninstall -y discord discord.py || true
-pip install py-cord==2.
+pip install py-cord==2.3.2
 pip install -r requirements.txt
